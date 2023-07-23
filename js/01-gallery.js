fbda45a7a76081a012533484ebccdb60acead791
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-07-23 15:21:12 +0200

    updates
    Updated tasks 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const img = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+img.insertAdjacentHTML("beforeend", markup);
+
+img.addEventListener("click", lightBoxModal);
+
+function lightBoxModal(e) {
+  e.preventDefault();
+  const target = e.target;
+  const image = target.dataset.source;
+  if (e.target.nodeName !== "a.gallery__link") {
+    const modal = basicLightbox.create(
+      `
+        <img width="1400" height="900" src="${image}">
+	`
+    );
+    modal.show();
+
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") modal.close();
+    });
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const img = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`
+  )
+  .join("");
+
+img.insertAdjacentHTML("beforeend", markup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
